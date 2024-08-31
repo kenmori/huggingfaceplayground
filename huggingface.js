4440046fc9f3a1a61514a0f02af16568bbd302f4
kenmori <kenjimoritata@gmail.com>
2024-09-01 05:58:04 +0900

    fix api

--- huggingface.js
+++ huggingface.js
@@ -5,19 +5,27 @@ dotenv.config();
 
 const HF_ACCESS_TOKEN = process.env.HF_ACCESS_TOKEN;
 
-const inference = new HfInference(HF_ACCESS_TOKEN);
+const hf = new HfInference(HF_ACCESS_TOKEN);
 
-const model = "Salesforce/blip-image-captioning-large";
-const imageUrl = "https://kenjimorita.jp/wp-content/uploads/2019/10/F11CB3D1-8522-43C7-9917-F96BD163E7B7.jpeg";
+// const model = "Salesforce/blip-image-captioning-large";
+// const imageUrl = "https://kenjimorita.jp/wp-content/uploads/2019/10/F11CB3D1-8522-43C7-9917-F96BD163E7B7.jpeg";
 
 
-const response = await fetch(imageUrl);
-const imageBlob = await response.blob();
+// const response = await fetch(imageUrl);
+// const imageBlob = await response.blob();
 
-const result = await inference.imageToText({
-  data: imageBlob,
-  model
-});
+// const result = await hf.imageToText({
+//   data: imageBlob,
+//   model
+// });
+
+const result = await hf.textToImage({
+  inputs: 'award winning high resolution photo of a giant tortoise/((ladybird)) hybrid, [trending on artstation]',
+  model: 'stabilityai/stable-diffusion-2',
+  parameters: {
+    negative_prompt: 'blurry',
+  }
+})
 
 
 console.log(result);
